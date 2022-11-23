fd2cb86316432b2fa758ba4fe03a1246db73eccd
GiulioGilardo <116483336+GiulioGilardo@users.noreply.github.com>
2022-11-23 19:04:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n, i, a, b, c;
+    cin >> n;
+    a=1;
+    b=1;
+    if (n>=2){
+        cout << a << endl << b << endl;
+        for (i=0; i+2<n; i++){
+            c=a+b;
+            cout << c << endl;
+            a=b;
+            b=c;
+        }
+
+    } else {
+        cout << "errore" << endl;
+    }
    return 0;
 }
